fc1e904b3e4fb2b7423a12fef10c8709842c7bd8
notalentgeek <mikael.pratama@yahoo.com>
2017-06-29 11:06:11 +0200

    Fixes in ut.bat and ut.sh.

--- ut.bat
+++ ut.bat
@@ -1,12 +1,12 @@
 @echo off
 
 cls
-python3 -B C:\\Users\\mikael\\pyautomation\\test_dbg.py
-python3 -B C:\\Users\\mikael\\pyautomation\\test_difi.py
-python3 -B C:\\Users\\mikael\\pyautomation\\test_dttz.py
-python3 -B C:\\Users\\mikael\\pyautomation\\test_img.py
-python3 -B C:\\Users\\mikael\\pyautomation\\test_note.py
-python3 -B C:\\Users\\mikael\\pyautomation\\test_op.py
-python3 -B C:\\Users\\mikael\\pyautomation\\test_pth.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_dbg.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_difi.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_dttz.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_img.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_note.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_op.py
+python3 -B C:\\Users\\mikael\\Downloads\\pyautomation\\test_pth.py
 
 pause
